a9b14347b8bd41667aeacc895cf3ae9a7b1067bc
Hwang <hwangdev97@gmail.com>
2022-04-19 16:19:00 +0800

    fix: remove GitHubShareButton

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -31,7 +31,6 @@ import { PageActions } from './PageActions'
 import { Footer } from './Footer'
 import { PageSocial } from './PageSocial'
 import { NotionPageHeader } from './NotionPageHeader'
-import { GitHubShareButton } from './GitHubShareButton'
 import { GraidentBackground } from './GradientBackground';
 
 import styles from './styles.module.css'
